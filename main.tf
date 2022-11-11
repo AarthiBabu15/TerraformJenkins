c09165558d12ed77ee009926aa454fc32616aaa5
AarthiBabu15 <117907429+AarthiBabu15@users.noreply.github.com>
2022-11-11 12:21:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,9 @@
-provider "aws" {
-  region     = "us-east-1"
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-
-}
-
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_public" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
-  subnet_id                   = aws_subnet.public-subnet-1.id
-  associate_public_ip_address = true
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
-  }
   tags = {
-    "Name" = "EC2-PUBLIC"
+    Name = "demo-vpc"
+    Purpose = "Jenkins Demo"
   }
-
-
 }
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_private" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
-  subnet_id                   = aws_subnet.private-subnet-1.id
-  associate_public_ip_address = false
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    "Name" = "EC2-PRIVATE"
-  }
-
-}
-################################
-
-
